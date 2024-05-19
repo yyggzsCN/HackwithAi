9aca1ff05005fb812af1f49909497fef6956dfb3
Aryan Patel <aryanpatel@v1040-wn-rt-a-40-202.campus-dynamic.uwaterloo.ca>
2024-05-19 14:31:24 -0400

    button

--- pythonapp.py
+++ pythonapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request
 import cv2
 
 app = Flask(__name__)
@@ -13,7 +13,7 @@ def generate_frames():
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
-                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
 def index():
@@ -22,6 +22,11 @@ def index():
 @app.route('/video_feed')
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/button_click', methods=['POST'])
+def button_click():
+    print("Button was clicked!")
+    return '', 204
 
 if __name__ == "__main__":
     app.run(debug=True)
